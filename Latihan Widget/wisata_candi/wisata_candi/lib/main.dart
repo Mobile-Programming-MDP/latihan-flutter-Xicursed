e1615770e659783150fa7fd584d25f432b72d81c
Xicursed <jeremiahpalembang2016@gmail.com>
2024-10-19 13:05:15 +0700

    Wisata_Candi

---
+++ Latihan Widget/wisata_candi/wisata_candi/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/DetailScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DetailScreen(
+        candi: candiList[0],
+      ),
+    );
+  }
+}
